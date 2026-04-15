c53728cc979186ecb424a67b50c544df1ae19ab0
shreyasinha13 <shreyasinha1309@gmail.com>
2026-04-15 19:23:42 +0530

    Added gitignore

--- backend/database.py
+++ backend/database.py
@@ -1,14 +1,19 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://localhost/employee_db"
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+from dotenv import load_dotenv
+import os
+
+# Load .env file
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-from sqlalchemy.orm import Session
-
+# Dependency
 def get_db():
     db = SessionLocal()
     try:
